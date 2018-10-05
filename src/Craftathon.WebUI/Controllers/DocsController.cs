1aeeb1d0bf113e6e6305ed6b0d3cc785d1ae8113
Michael Tse <tsem@emea.cshare.net>
2018-10-05 14:59:16 +0100

    Update

---
+++ src/Craftathon.WebUI/Controllers/DocsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Craftathon.WebUI.Controllers
+{
+    public class DocsController : Controller
+    {
+        [Route("/docs/continuous-integration")]
+        [HttpGet]
+        public IActionResult ContinuousIntegration()
+        {
+            return View("../Docs/ContinuousIntegration");
+        }
+
+        [Route("/docs/continuous-deployment")]
+        [HttpGet]
+        public IActionResult ContinuousDeployment()
+        {
+            return View("../Docs/ContinuousDeployment");
+        }
+    }
+}
---
+++ src/Craftathon.WebUI/Controllers/ExampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Craftathon.WebUI.Controllers
+{
+    public class ExampleController : Controller
+    {
+        [Route("/example/http-trigger")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("../Examples/HttpTrigger");
+        }
+    }
+}
